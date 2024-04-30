cab4ec966baf0167b0066cc54a99ddefd8bd39a5
Lian <markliandperez61@gmail.com>
2024-04-30 12:43:21 +0800

    add eslint

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
-import React from "react";
-import { View } from "react-native";
-import Home from './androidhelp/download_Android'
+import React from 'react';
+import {View} from 'react-native';
+import Home from './androidhelp/download_Android';
 
-const App = () =>{
-
-    return(
-<View>
-    <Home/>
-</View>
-    );
+const App = () => {
+  return (
+    <View>
+      <Home />
+    </View>
+  );
 };
 
 export default App;
--- androidhelp/download_Android.js
+++ androidhelp/download_Android.js
@@ -6,55 +6,54 @@ const AndroidHelp = () => {
   const header = require('../Image/header.jpg');
   const bg = require('../Image/bg.png');
   const backimg = require('../Image/back-removebg-preview.png');
-// Images Tutorials
-const playstore = require('../Image/playstore.png');
-const search = require('../Image/search.png');
-const finish = require('../Image/finishdl.png');
-const open = require('../Image/open.png');
+  // Images Tutorials
+  const playstore = require('../Image/playstore.png');
+  const search = require('../Image/search.png');
+  const finish = require('../Image/finishdl.png');
+  const open = require('../Image/open.png');
 
   return (
     <ScrollView>
-    
       <SafeAreaView style={styles.content}>
-        <View style = {styles.title}>
-        <Text style={styles.AndroidTitle}> Android Download Help</Text>
+        <View style={styles.title}>
+          <Text style={styles.AndroidTitle}> Android Download Help</Text>
+        </View>
+        <View style={styles.tutorials}>
+          <Text style={styles.textstyle}>
+            1. On Your Android Device, tap the Play {'\n'}
+            Store icon open the App
+          </Text>
+          <Image source={playstore} style={styles.openplay} />
 
-       
-        </View>
-       <View style = {styles.tutorials}>
-       <Text style = {styles.textstyle}>1. On Your Android Device, tap the Play {'\n'}
-       Store icon open the App
-        </Text>
-        <Image source={playstore} style={styles.openplay}/>
-      
-     <View style = {styles.step2}>
-        <Text style = {styles.textstyle}>2. Search for GoPlay360 in the search{'\n'} bar  
-        at the top of the screen </Text>
-<Image source={search} style = {styles.search}/>
-        </View>
+          <View style={styles.step2}>
+            <Text style={styles.textstyle}>
+              2. Search for GoPlay360 in the search{'\n'} bar at the top of the
+              screen{' '}
+            </Text>
+            <Image source={search} style={styles.search} />
+          </View>
 
-        <View style = {styles.step3}>
-        <Text style = {styles.textstyle}>3. Click the install and once  it finish {'\n'}  it show this 
-       </Text>
-<Image source={finish} style = {styles.open}/>
-        </View>
+          <View style={styles.step3}>
+            <Text style={styles.textstyle}>
+              3. Click the install and once it finish {'\n'} it show this
+            </Text>
+            <Image source={finish} style={styles.open} />
+          </View>
 
-        <View style = {styles.step4}>
-        <Text style = {styles.textstyle}>4. Tap on the open button to lunch {'\n'} 
-        the GoPlay360 </Text>
-<Image source={open} style = {styles.open}/>
+          <View style={styles.step4}>
+            <Text style={styles.textstyle}>
+              4. Tap on the open button to lunch {'\n'}
+              the GoPlay360{' '}
+            </Text>
+            <Image source={open} style={styles.open} />
+          </View>
         </View>
-       </View>
-       <View>
-       
-       </View>
+        <View />
       </SafeAreaView>
       <Image source={bg} style={styles.background} />
       <Image source={bg} style={styles.background} />
       <Image source={backimg} style={styles.back} />
       <Image source={header} style={styles.header} />
-
-
     </ScrollView>
   );
 };
